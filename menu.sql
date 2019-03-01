86b025c68468907ff1593e24bf681dab622ed5cf
Natosha <nmsuri28@gmail.com>
2019-03-01 16:06:31 -0600

    Added a new colomun to two tables, added a new schema, updated Readme

---
+++ menu.sql
@@ -0,0 +1,24 @@
+
+--Notes-
+--CATEGORY_id REFERENCE FOR PRODUCTS TABLE--
+--category_id: non-alcholic drinks: 1, alcholic drinks: 2, appetizers: 3, entrees: 4, desserts: 5, specials: 6, kids menu:7, sides: 8, party platters:9, party platters add ons: 10--
+insert into products(name, discount_price, product_image, category_id, description)
+value("The Boss Burger", 12.99, "https://static.olocdn.net/menu/chilis/e5d370536e8f23c6150aa25d0ca48c06.jpg", 4,"The burger all other burgers report to. Smoked brisket, tender rib meat, jalapeno-cheddar smoked sausage, bacon & cheddar with lettuce, tomato, House BBQ & house-made ranch. We. Dare. You.");
+
+insert into products(name, discount_price, product_image, category_id, description)
+value("Mushroom Swiss Burger", 12.99, "https://static.olocdn.net/menu/chilis/80805def7db84dec6c9a08d1ebfb5ee6.jpg", 4, "There’s so mush-room in our hearts for this one. Topped with sauteed onions, mushrooms, Swiss, lettuce, tomato & garlic aioli.");
+
+insert into products(name, discount_price, product_image, category_id, description)
+value("Queso Burger", 11.99, "https://static.olocdn.net/menu/chilis/1e7dfda0500e068313d3afbd73317cb8.jpg", 4, "Say “cheese!” This half-pound patty comes smothered in white queso, crunchy tortilla strips & pico.");
+
+insert into products(name, discount_price, product_image, category_id, description)
+value("Southern Smokehouse Burger", 12.99, "https://static.olocdn.net/menu/chilis/185b4c044ae4bb8e4b3d3d1e76502531.jpg", 4, "Bacon, cheddar, pickles, Awesome Blossom Petals, lettuce, tomato, garlic aioli. Served with a side of original BBQ sauce.");
+
+insert into products(name, discount_price, product_image,category_id, description)
+value("Big Mouth Bites", 10.99, "https://static.olocdn.net/menu/chilis/4316e7d8004b1ad6ea7abfbe370619e4.jpg", 4, "4 mini burgers with bacon, American cheese, sauteed onions, house-made ranch.");
+
+insert into products(name, discount_price, product_image,category_id, description)
+value("Soft Drinks, Teas, & Lemonades", 2.99, "https://static.olocdn.net/menu/chilis/7018cb063e002630dfc14d6f5bca85e8.jpg", 1, "Enjoy our house-made lemonades, teas & soft drinks.");
+
+insert into products(name, discount_price, product_image,category_id, description)
+value("Pizza", 5.99, "https://media.olivegarden.com/en_us/images/product/KidsPepperoniPizza_gv1x1.jpg", 7, "Cheese Pizza" );
