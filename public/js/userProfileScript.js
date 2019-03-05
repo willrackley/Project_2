95be9336e7d8a01e9b861025484f5f1d2b2a915b
williamrackley <willrackley65@gmail.com>
2019-03-05 11:46:29 -0500

    working on user ordering functionality, still need to post the order to table and display in checkout screen

--- public/js/userProfileScript.js
+++ public/js/userProfileScript.js
@@ -1,3 +1,5 @@
+var cart = [];
+
 $('#userScanQRcode').on('click', function(){
 
     var menuItems;
@@ -33,20 +35,37 @@ $('#userScanQRcode').on('click', function(){
         var buttonDiv = $('<div>');
         buttonDiv.addClass("mt-2")
         var addBtn = $('<button>');
+        addBtn.addClass("addBtn");
+        addBtn.attr("itemName", menuItem.name);
+        addBtn.attr("key", menuItem.id);
+        addBtn.attr("itemPrice", menuItem.discount_price);
         var addSymbol = $('<i class="fas fa-plus">');
         addSymbol.addClass("p-2 text-dark");
-        addBtn.addClass("")
+        var input = $('<input type="number" id="itemAmountInput' + menuItem.id +  '"  class="mr-3 text-center" min="1" value="1" max="5">');
         addBtn.append(addSymbol);
+        buttonDiv.append(input);
         buttonDiv.append(addBtn);
         cardBody.append(buttonDiv);
         card.append(cardHeader);
         card.append(cardBody);
-        
         $('#menuContainer').append(card);
     }
-
     getMenu();
-
-
-
 });
+
+$(document).on('click', '.addBtn', function(){
+    var inputKey = $(this).attr('key');
+    var inputSelector = '#itemAmountInput' + inputKey;
+    var inputAmount = parseInt($(inputSelector).val());
+
+    var itemChoice = {
+        name: $(this).attr('itemName'),
+        price: $(this).attr('itemPrice'),
+        amount: inputAmount
+     };
+
+    cart.push(itemChoice);
+    console.log(cart);
+    //still need to list the cart items in the checkout page
+    //and add to orders table
+}); 
