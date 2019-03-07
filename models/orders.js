97ca71a760bbd073a7f8a8a6b9fd932b1d45d834
williamrackley <willrackley65@gmail.com>
2019-03-06 22:09:34 -0500

    styling to user payment page and form added

--- models/orders.js
+++ models/orders.js
@@ -1,7 +1,7 @@
 module.exports = function(sequelize, DataTypes){
     var Orders = sequelize.define("Orders",{
-        order_id: DataTypes.INTEGER,
         order_user_id: DataTypes.INTEGER,
+        menu_id: DataTypes.INTEGER,
         status: DataTypes.STRING,
         date: DataTypes.DATE,
         comment: DataTypes.STRING
