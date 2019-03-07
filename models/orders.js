8c647662bc0cb7e1cf3c742895352299aa826475
germangib <german.garcia@icloud.com>
2019-03-07 02:40:56 -0500

    Adding /public/scripts/customerOrderScript.js; public/customerOrders.html; modified models/orders; routes/orders

--- models/orders.js
+++ models/orders.js
@@ -5,5 +5,26 @@ module.exports = function(sequelize, DataTypes){
         date: DataTypes.DATE,
         comment: DataTypes.STRING
     });
+
+    // Added by GG - 03/07 
+    // Testing associations of Orders table
+
+    Orders.associate = function(models){
+        //Associate orders with products 
+        // one order has many products
+        Orders.hasMany(models.Products, {
+            onDelete: "cascade"
+        });
+
+        //Associate orders to order details
+        // one order has only one order detail
+        Orders.hasOne(models.DetailedOrder, {
+            onDelete: "cascade"
+        });
+    }; 
+
+    // End of changes 
+
+    
     return Orders;
 }
