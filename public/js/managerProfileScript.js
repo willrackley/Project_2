77ac90ee0a5d5acc0074a9721e7fae3d0ac254f6
williamrackley <willrackley65@gmail.com>
2019-03-08 17:46:34 -0500

    displays current orders in mangagers dashboard

--- public/js/managerProfileScript.js
+++ public/js/managerProfileScript.js
@@ -1,3 +1,5 @@
+$(document).ready(function(){
+
 var menuItems;
 
     //function that grabs all api information
@@ -40,14 +42,73 @@ var menuItems;
         tableRow.append(price);
         tableRow.append(category);
         tableRow.appendTo($('#tableBody'));
-       
-       
 
     }
+
+    var orderData;
+    var ordersColumn = [];
+    function getOrders(){
+        $.get('/app/orders/kitchen', function(data){
+            orderData = data;
+            console.log(orderData);
+            initializeOrderData();
+        });
+    }
+
+    function initializeOrderData(){
+       var ordersToAdd = [];
+       ordersColumn = [];
+       for (var i = 0; i < orderData.length; i++) {
+           ordersToAdd.push(createOrderDiv(orderData[i]));
+        }
+        $('#orderContainer').append(ordersToAdd);
+    }
+
+    function createOrderDiv(orderData){
+        //console.log('made it')
+        ordersColumn = [];
+        var card = $('<div class="card menuCards mb-5">');
+        var cardHeader = $('<div class="card-header kitchenCardHeader w-100">');
+        var cardBody = $('<div class="card-body p-3">');
+        var status = $('<div class="mt-1">');
+        status.text('STATUS: pending');
+        var btnDiv = $('<div class="text-center">')
+        var redBtn = $('<button class="btn btn-danger RedButton mx-1 mt-2">');
+        redBtn.attr('id', 'kitchen-action');
+        redBtn.attr('value', orderData.id);
+        redBtn.text('CANCELED');
+        var yellowBtn = $('<button class="btn mx-1 btn-warning YellowButton mt-2">');
+        yellowBtn.attr('id', 'kitchen-action');
+        yellowBtn.attr('value', orderData.id);
+        yellowBtn.text('IN PROCCESS');
+        var greenBtn = $('<button class="btn mx-1 btn-success GreenButton mt-2">');
+        greenBtn.attr('id', 'kitchen-action');
+        greenBtn.attr('value', orderData.id);
+        greenBtn.text('COMPLETED');
+        cardHeader.text('ORDER: ' + orderData.id);
+        for(var i=0; i < orderData.detailOrders.length; i++ ){
+            var quantity = orderData.detailOrders[i].quantity
+            var product = orderData.detailOrders[i].Product.name
+            ordersColumn.push(quantity);
+            ordersColumn.push(product);
+        }
+        cardBody.text(ordersColumn.join(['\xa0 \xa0']));
+        status.appendTo(cardHeader);
+        redBtn.appendTo(btnDiv);
+        yellowBtn.appendTo(btnDiv);
+        greenBtn.appendTo(btnDiv);
+        btnDiv.appendTo(cardBody);
+        cardHeader.appendTo(card);
+        //ordeeOrderData.text(ordersColumn.join(['\xa0 \xa0']));
+        cardBody.appendTo(card)
+        $('#orderContainer').append(card);
+       
+    }
+
+    getOrders();
     getMenu();
 
     // Get Categories and Products From Database And Add To Select List
-$(document).ready(function(){
     $.get('/app/products/categories', function(data) {
         for (var i=0; i < data.length; i++) {
             var option =$('<option>');
@@ -66,7 +127,7 @@ $(document).ready(function(){
             option.appendTo($("#select-product"));
         }
     });
-});
+
 
 $('#select-product').on('change', function() {
     for (var i=0; i < menuItems.length; i++) {
@@ -79,6 +140,8 @@ $('#select-product').on('change', function() {
             $('#itemDecription').val(menuItems[i].description);
         }
     }
+});
+
 });
 
     
